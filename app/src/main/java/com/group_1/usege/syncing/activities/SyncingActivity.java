05fd4b7c75adadbd630182e3040cc198d03081a3
nguyenthienphu1206 <79633694+nguyenthienphu1206@users.noreply.github.com>
2023-03-21 20:56:21 +0700

    UI - Load data from system

---
+++ app/src/main/java/com/group_1/usege/syncing/activities/SyncingActivity.java
@@ -0,0 +1,47 @@
+package com.group_1.usege.syncing.activities;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.group_1.usege.R;
+
+public class SyncingActivity extends AppCompatActivity {
+
+    TextView text_view_sync;
+    ImageView image_view_backward;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_syncing);
+
+        text_view_sync = findViewById(R.id.txtView_sync);
+        text_view_sync.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickOpenSetUpSyncingBottomSheetDialog();
+            }
+        });
+    }
+
+    private void clickOpenSetUpSyncingBottomSheetDialog() {
+        View viewDialog = getLayoutInflater().inflate(R.layout.layout_set_up_syncing, null);
+
+        final BottomSheetDialog setUpSyncingBottomSheetDialog = new BottomSheetDialog(this);
+        setUpSyncingBottomSheetDialog.setContentView(viewDialog);
+        setUpSyncingBottomSheetDialog.show();
+
+        image_view_backward = viewDialog.findViewById(R.id.image_view_backward);
+        image_view_backward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setUpSyncingBottomSheetDialog.dismiss();
+            }
+        });
+    }
+}
