17a46f14e6b8d0dd75e48d96e41ab8de82d2a511
risethesummer <honhatlinhmail@gmail.com>
2023-04-12 19:18:53 +0700

    add user plan and statistic activity

---
+++ app/src/main/java/com/group_1/usege/userInfo/activities/UserPlanActivity.java
@@ -0,0 +1,20 @@
+package com.group_1.usege.userInfo.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.group_1.usege.R;
+
+public class UserPlanActivity extends AppCompatActivity {
+
+    public UserPlanActivity()
+    {
+        super(R.layout.activity_user_plan);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/group_1/usege/userInfo/activities/UserStatisticActivity.java
@@ -0,0 +1,21 @@
+package com.group_1.usege.userInfo.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.group_1.usege.R;
+
+public class UserStatisticActivity extends AppCompatActivity {
+
+    public UserStatisticActivity()
+    {
+        super(R.layout.activity_user_statistic);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
