d8ea1b8014568c81e73733ca15f6c5d9e410024c
nguyenthienphu1206 <79633694+nguyenthienphu1206@users.noreply.github.com>
2023-04-20 21:47:47 +0700

    Post api

--- app/src/main/java/com/group_1/usege/api/apiservice/ApiService.java
+++ app/src/main/java/com/group_1/usege/api/apiservice/ApiService.java
@@ -3,8 +3,14 @@ package com.group_1.usege.api.apiservice;
 
 import com.group_1.usege.api.googlemaps.GeocodeResponse;
 
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
 import retrofit2.http.Query;
 
 public interface ApiService {
@@ -13,4 +19,8 @@ public interface ApiService {
     Call<GeocodeResponse> getLocation(@Query("latlng") String latlng,
                                       @Query("key") String apiKey);
 
+    @Multipart
+    @POST("upload")
+    Call<ResponseBody> uploadFile(@Part("userId")RequestBody userId,
+                                  @Part MultipartBody.Part image);
 }
---
+++ app/src/main/java/com/group_1/usege/api/apiservice/ApiUploadFile.java
@@ -0,0 +1,4 @@
+package com.group_1.usege.api.apiservice;
+
+public class ApiUploadFile {
+}
