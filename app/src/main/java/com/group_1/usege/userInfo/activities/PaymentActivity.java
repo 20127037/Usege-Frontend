5b6d180a9f2bffc9d55280c0172d359e50a4981b
risethesummer <honhatlinhmail@gmail.com>
2023-04-20 12:18:53 +0700

    add payment activity

---
+++ app/src/main/java/com/group_1/usege/userInfo/activities/PaymentActivity.java
@@ -0,0 +1,14 @@
+package com.group_1.usege.userInfo.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.group_1.usege.R;
+
+public class PaymentActivity extends AppCompatActivity {
+
+    public PaymentActivity()
+    {
+        super(R.layout.activity_payment);
+    }
+
+}
