6f5df13860d98b8101b250605e7510e320cf5770
risethesummer <honhatlinhmail@gmail.com>
2023-04-28 21:34:33 +0700

    adapt online library layout to design document

--- app/src/main/java/com/group_1/usege/library/activities/OnlineLibraryActivity.java
+++ app/src/main/java/com/group_1/usege/library/activities/OnlineLibraryActivity.java
@@ -7,39 +7,71 @@ import android.widget.ImageView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.core.view.GravityCompat;
+import androidx.core.widget.NestedScrollView;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.navigation.NavigationView;
 import com.group_1.usege.R;
 
 public class OnlineLibraryActivity extends AppCompatActivity {
+
+    private SearchView searchView;
+    private RecyclerView recyclerView;
+
+    public OnlineLibraryActivity()
+    {
+        super(R.layout.activity_online_library);
+    }
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_online_library);
+        searchView = findViewById(R.id.view_search);
+        recyclerView = findViewById(R.id.lst_img);
+        recyclerView.setAdapter();
 
-        NavigationView rootNavigationView = findViewById(R.id.root_navigation_view);
-        rootNavigationView.setNavigationItemSelectedListener(
-                new NavigationView.OnNavigationItemSelectedListener() {
-                    @Override
-                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                        return false;
-                    }
-                });
-        ImageView rootMenuImageView = findViewById(R.id.root_menu_image_view);
-        rootMenuImageView.setOnClickListener(v -> {
-            DrawerLayout drawerLayout = findViewById(R.id.root_drawer_layout);
-            if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-                drawerLayout.closeDrawer(GravityCompat.START);
-            } else {
-                drawerLayout.openDrawer(GravityCompat.START);
-            }
-        });
-    }
+        ViewModelProvider viewModel = new ViewModelProvider(this)
+                .get(ExampleViewModel.class);
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
+        UserAdapter pagingAdapter = new UserAdapter(new UserComparator());
+        recyclerView.adapter = pagingAdapter
+        viewModel.flowable
+                // Using AutoDispose to handle subscription lifecycle.
+                // See: https://github.com/uber/AutoDispose
+                .to(autoDisposable(AndroidLifecycleScopeProvider.from(this)))
+                .subscribe(pagingData -> pagingAdapter.submitData(lifecycle, pagingData));
     }
+//
+//    @Override
+//    protected void onCreate(@Nullable Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView();
+//
+//        NavigationView rootNavigationView = findViewById(R.id.root_navigation_view);
+//        rootNavigationView.setNavigationItemSelectedListener(
+//                new NavigationView.OnNavigationItemSelectedListener() {
+//                    @Override
+//                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                        return false;
+//                    }
+//                });
+//        ImageView rootMenuImageView = findViewById(R.id.root_menu_image_view);
+//        rootMenuImageView.setOnClickListener(v -> {
+//            DrawerLayout drawerLayout = findViewById(R.id.root_drawer_layout);
+//            if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+//                drawerLayout.closeDrawer(GravityCompat.START);
+//            } else {
+//                drawerLayout.openDrawer(GravityCompat.START);
+//            }
+//        });
+//    }
+//
+//    @Override
+//    protected void onDestroy() {
+//        super.onDestroy();
+//    }
 }
